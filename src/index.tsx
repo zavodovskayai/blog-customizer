b0ff5d49bea331d4b38ab3423f8a1dd6875e52b5
zavodovskayai <118307042+zavodovskayai@users.noreply.github.com>
2025-05-06 10:23:56 +0300

    Так как app компонент вне папки, то изменила путь

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import { App } from './components/app/app';
+import { App } from './components/app';
 
 import './styles/index.scss';
 
